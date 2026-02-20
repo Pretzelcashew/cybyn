405f7973db6e4c65b0e974bfc8f6d780d3776b04
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-20 01:42:41 +0000

    removed outdated comment to random int

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ document.querySelector('.main-text').textContent = options[random_num];
 
 /**range excludes max number*/
 function getRandomInt(min, max) {
-  // +1 makes it "inclusive" (so it can actually hit the max number)
   return Math.floor(Math.random() * (max - min)) + min;
 }
