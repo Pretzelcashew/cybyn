95896b7a8b0311d6df652319d4bf214c821864e5
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-20 03:11:13 +0000

    update index.html, main.js and styles.css

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let options = ['minerals', 'halogen', 'gelatin'];
 random_num = getRandomInt(0,options.length);
 document.querySelector('.main-text').textContent = options[random_num];
 
+let ctx = document.getElementById('main-canvas');
+
+
 /**range excludes max number*/
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
