2953745c55411ca8f3e660312c1c8893cf243d85
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-20 01:41:21 +0000

    tweaked random to exclude max number

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let options = ['minerals', 'halogen', 'gelatin'];
-let random_num = Math.floor(Math.random() * options.length);
+random_num = getRandomInt(0,options.length);
 document.querySelector('.main-text').textContent = options[random_num];
 
+/**range excludes max number*/
 function getRandomInt(min, max) {
   // +1 makes it "inclusive" (so it can actually hit the max number)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
