0ef77ed98112edc1f81de931d1b2eac65773de64
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-20 01:23:26 +0000

    added random

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.querySelector('.main-text').textContent = "Loaded!";
+let options = ['minerals', 'halogen', 'gelatin'];
+let random_num = getRandomInt(0,options.length);
+document.querySelector('.main-text').textContent = options[random_num];
+
+function getRandomInt(min, max) {
+  // +1 makes it "inclusive" (so it can actually hit the max number)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
