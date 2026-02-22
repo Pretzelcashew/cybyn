dd1e1cba53d7cadd98844748cee4ad0ec65f421c
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-22 09:37:41 -0500

    added mint fill to canvas

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ document.querySelector('.main-text').textContent = options[random_num];
 
 let canvas = document.getElementById('main-canvas');
 let ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 400;
+
+ctx.fillStyle = "rgb(182, 255, 203)";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
 ctx.fillStyle = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, ${Math.random()})`;
 ctx.fillRect(getRandomInt(25,100), getRandomInt(25,100), getRandomInt(1,100), getRandomInt(1,100));
 
