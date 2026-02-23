76e495e2bb4d5498d7314c9b4629bf4bdbbcb6b1
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-02-23 13:29:49 -0500

    lots of anti hugging attemps

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
+const wait = ms => new Promise(res => setTimeout(res, ms));
+
+
+document.body.insertAdjacentHTML('afterbegin', '<div>I am guaranteed to appear, and I changed, again 9.2!</div>');
+
+
+
+
+
 let options = ['minerals', 'halogen', 'gelatin'];
 let random_num = getRandomInt(0,options.length);
 document.querySelector('.main-text').textContent = options[random_num];
 
 let canvas = document.getElementById('main-canvas');
 let ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 400;
+
 
 ctx.fillStyle = "rgb(182, 255, 203)";
 ctx.fillRect(0,0,canvas.width,canvas.height);
-for (let i = 0; i < 50; i++) {
-ctx.fillStyle = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, ${Math.random()})`;
+for (let a = 0; a < 500000; a++) {
+  for (let b = 0; b < 1; b++) {
+ctx.fillStyle = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, 1)`;
 ctx.fillRect(getRandomInt(0,canvas.width), getRandomInt(0,canvas.height), getRandomInt(1,100), getRandomInt(1,100));
-document.body.insertAdjacentHTML('beforeend', '<div>I am guaranteed to appear, and I changed, again!</div>');
+  }
+await wait(1);
+
 }
 
 
